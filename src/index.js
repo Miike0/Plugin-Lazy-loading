a0b40b5f3a70f65c946047de7a3be1be12fe7762
Miikeo <Miguel.A.INCO@gmail.com>
2022-11-10 21:26:52 -0600

    Added necessary nodes, got images from unsplash API; using a random number added at the end of the API URL call to get different photos on each call and added a button to add more images to the page

--- src/index.js
+++ src/index.js
@@ -3,4 +3,34 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-console.log('Happy hacking :)')
+//  https://gersonlazaro.com/unsplash-api-miles-de-fotos-gratis-en-tu-sitio-web-o-aplicacion/
+
+const mountNode = document.getElementById('images');
+const addButton = document.querySelector('.add-button');
+
+const unsplashApi = 'https://source.unsplash.com/random/';
+console.log(unsplashApi)
+
+const maximum = 100000;
+const minimum = 1;
+const random = () => Math.floor (Math.random() * (maximum - minimum)) + minimum;
+
+const createImageNode = () => {
+    const imageContainer = document.createElement('div');
+    imageContainer.className = 'p-4';
+
+    const image = document.createElement('img');
+    image.className = 'max-auto';
+    image.width = '320';
+    image.src = `${unsplashApi}${random()}`;
+
+    imageContainer.appendChild(image);
+    return imageContainer;
+}
+
+const addImage = () => {
+    const newImage = createImageNode();
+    mountNode.append(newImage);
+}
+
+addButton.addEventListener('click', addImage);
